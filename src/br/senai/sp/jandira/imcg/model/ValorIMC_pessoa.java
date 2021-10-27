f34e73d2716c47e9cb6f360f2c50ec1e591cf9ae
samueltank <dev.samueltank@gmail.com>
2021-10-27 11:53:44 -0300

    Alteração da posição do resultado.

--- src/br/senai/sp/jandira/imcg/model/ValorIMC_pessoa.java
+++ src/br/senai/sp/jandira/imcg/model/ValorIMC_pessoa.java
@@ -1,62 +1,68 @@
 package br.senai.sp.jandira.imcg.model;
 
+import java.text.DecimalFormat;
+
 abstract class IMC {
-    public abstract void calcular(String peso, String altura);
-    protected double peso;
-    protected double altura;
-    protected double resultadoIMC;
+    public abstract void calculate(String peso, String altura);
+    protected double weight;
+    protected double height;
+    protected String resultIMC;
 
-    public void setPeso(double peso) {
-        this.peso = peso;
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
-    public double getPeso() {
-        return peso;
+    public double getWeight() {
+        return weight;
     }
 
-    public void setAltura(double altura) {
-        this.altura = altura;
+    public void setHeight(double height) {
+        this.height = height;
     }
 
-    public double getAltura() {
-        return altura;
+    public double getHeight() {
+        return height;
     }
 
-    public void setResultadoIMC(double resultadoIMC) {
-        this.resultadoIMC = resultadoIMC;
+    public void setResultIMC(String resultIMC) {
+        this.resultIMC = resultIMC;
     }
 
-    public double getResultadoIMC() {
-        return resultadoIMC;
+    public String getResultIMC() {
+        return resultIMC;
     }
 
     public String getIMC_String() {
-        return String.valueOf(getResultadoIMC());
+        return String.valueOf(getResultIMC());
     }
 
     public String CalcularSaidaText() {
-        if (getResultadoIMC() >= 40) {
+        double resultIMC = Double.parseDouble(getResultIMC());
+        if (resultIMC >= 40) {
             return "Obesidade III";
-        } else if (getResultadoIMC() >= 35 && getResultadoIMC() <= 39.9) {
+        } else if (resultIMC >= 35 && resultIMC <= 39.9) {
             return "Obesidade  II";
-        } else if (getResultadoIMC() >= 30 && getResultadoIMC() <= 34.9) {
+        } else if (resultIMC >= 30 && resultIMC <= 34.9) {
             return "Obesidade I";
-        } else if (getResultadoIMC() >= 25 && getResultadoIMC() <= 29.9) {
+        } else if (resultIMC >= 25 && resultIMC <= 29.9) {
             return "Levemente acima do peso";
-        } else if (getResultadoIMC() >= 18.6 && getResultadoIMC() <= 24.9) {
+        } else if (resultIMC >= 18.6 && resultIMC <= 24.9) {
             return "Peso ideal (Parabéns!)";
         } else {
             return "Abaixo do peso";
         }
     }
-
 }
 
 public class ValorIMC_pessoa extends IMC {
-    public void calcular(String altura, String peso) {
-        setPeso(Double.parseDouble(peso));
-        setAltura(Double.parseDouble(altura));
+    public void calculate(String heigth, String weigth) {
+        setWeight(Double.parseDouble(weigth));
+        setHeight(Double.parseDouble(heigth));
 
-        resultadoIMC = (this.peso / Math.pow(this.altura, 2));
+        // Formatador:
+        DecimalFormat df = new DecimalFormat("0.##");
+
+        double result = (this.weight / Math.pow(this.height, 2));
+        resultIMC = df.format(result);
     }
 }
--- src/br/senai/sp/jandira/imcg/ui/Tela.java
+++ src/br/senai/sp/jandira/imcg/ui/Tela.java
@@ -10,16 +10,20 @@ import java.awt.event.MouseListener;
 public class Tela {
     public void criarTela() {
 
+        // Criação de fontes:
         Font titleFont = new Font("Arial", Font.BOLD, 28);
+        Font status = new Font("Arial", Font.ITALIC, 18);
+
         // Criação do objeto pessoaIMC:
         ValorIMC_pessoa samuel = new ValorIMC_pessoa();
 
-        JFrame telaIcm = new JFrame();
-        telaIcm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /*  <- fecha o
+        // Criação do objeto janela:
+        JFrame telaIMC = new JFrame();
+        telaIMC.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /*  <- fecha o
         programa quando a janela é fechada; */
-        telaIcm.setSize(280, 400);
-        telaIcm.setTitle("Calculadora de IMC");
-        telaIcm.setLayout(null);
+        telaIMC.setSize(350, 400);
+        telaIMC.setTitle("Calculadora de IMC");
+        telaIMC.setLayout(null);
 
         // criar um label:
         JLabel lableTitle = new JLabel("Calcular I.M.C");
@@ -35,37 +39,47 @@ public class Tela {
         labelIMC.setBounds(15, 260, 150, 24);
         JLabel labelSaida = new JLabel("Sua situação: ");
         labelSaida.setBounds(15, 294, 150, 24);
+        JLabel lblOutputIMC = new JLabel(" ");
+        lblOutputIMC.setFont(status);
+        lblOutputIMC.setForeground(Color.green);
+        lblOutputIMC.setBounds(140, 260, 150, 24);
+        JLabel lblOutputStatus = new JLabel(" ");
+        lblOutputStatus.setFont(status);
+        lblOutputStatus.setForeground(Color.green);
+        lblOutputStatus.setBounds(140, 295, 200, 24);
 
         // criação do jtext:
         JTextField textFieldAltura = new JTextField();
-        textFieldAltura.setBounds(145, 110, 100, 24);
+        textFieldAltura.setBounds(140, 110, 100, 24);
         JTextField textFieldPeso = new JTextField();
-        textFieldPeso.setBounds(145, 160, 100, 24);
+        textFieldPeso.setBounds(140, 160, 100, 24);
 
 
         // criar um botão:
         JButton buttonVerificar = new JButton();
         buttonVerificar.setText("Calcular");
-        buttonVerificar.setBounds(70, 210, 120, 35);
+        buttonVerificar.setBounds(50, 210, 150, 35);
 
         // colocar o label na área de conteúdo:
-        telaIcm.getContentPane().add(lableTitle);
-        telaIcm.getContentPane().add(labelAltura);
-        telaIcm.getContentPane().add(labelPeso);
-        telaIcm.getContentPane().add(buttonVerificar);
-        telaIcm.getContentPane().add(textFieldAltura);
-        telaIcm.getContentPane().add(textFieldPeso);
-        telaIcm.getContentPane().add(labelIMC);
-        telaIcm.getContentPane().add(labelSaida);
+        telaIMC.getContentPane().add(lableTitle);
+        telaIMC.getContentPane().add(labelAltura);
+        telaIMC.getContentPane().add(labelPeso);
+        telaIMC.getContentPane().add(buttonVerificar);
+        telaIMC.getContentPane().add(textFieldAltura);
+        telaIMC.getContentPane().add(textFieldPeso);
+        telaIMC.getContentPane().add(labelIMC);
+        telaIMC.getContentPane().add(labelSaida);
+        telaIMC.getContentPane().add(lblOutputIMC);
+        telaIMC.getContentPane().add(lblOutputStatus);
 
-        telaIcm.setVisible(true); // sempre no final do código;
+        telaIMC.setVisible(true); // sempre no final do código;
 
         // definir ouvinte(listener) do click do mouse:
         buttonVerificar.addMouseListener(new MouseListener() {
             @Override public void mouseClicked(MouseEvent e) {
-                samuel.calcular(textFieldAltura.getText(), textFieldPeso.getText());
-                labelIMC.setText(samuel.getIMC_String());
-                labelSaida.setText(samuel.CalcularSaidaText());
+                samuel.calculate(textFieldAltura.getText(), textFieldPeso.getText());
+                lblOutputIMC.setText(samuel.getResultIMC());
+                lblOutputStatus.setText(samuel.CalcularSaidaText());
             }
 
             @Override public void mousePressed(MouseEvent e) {
